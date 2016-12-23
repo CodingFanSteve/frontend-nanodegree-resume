1261434070ee0f4834703bf5ab5f88e0e08dfea9
yaosteveliu <yaosteveliu@gmail.com>
2016-12-22 16:17:57 -0800

    Partial refactor

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,24 +1,19 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+const placeholder = "%data%";
 
-var placeholder = "%data%";
-
- var bio = {
-	"name" :"Coding Fan Steve",
-	"role" : "CEO",
-	"contacts" : 
-	{
-		"email" : "codingfansteve@google.com",
-		"location" : "Mountain View, CA",
-		"mobile" : "12345"
+var model = {
+	bio : {
+		"name" :"Coding Fan Steve",
+		"role" : "CEO",
+		"contacts" : 
+		{
+			"email" : "codingfansteve@google.com",
+			"location" : "Mountain View, CA",
+			"mobile" : "12345"
+		},
+		"skills" : [ "coding", "management", "leadership" ]
 	},
-	"skills" : [ "coding", "management", "leadership" ]
-};
 
-var works = {
-	"jobs" : 
-	[
+	jobs : [
 		{
 	 		"employer" : "LinkedIn",
 	 		"title" : "Software Engineer",
@@ -47,69 +42,105 @@ var works = {
 	 		"dates" : "summer 2011",
 	 		"description" : "Built something at Oracle"
 	 	}
-	]
-};
-
-var educations = {
-	"schools" : 
-	[
-		{
-			"school" : "UC Berkley",
-			"location" : "Berkley, CA",
-			"degree" : "Ph.D.",
-			"major" : "Computer Science"
-		},
-		{
-			"school" : "MIT",
-			"location" : "Cambridge, MA",
-			"degree" : "BS",
-			"major" : "Electrical Engineering"
-		}
 	],
-	"Online Course" : ["Udacity"]
-};
 
-var projects = {
-	"projects" : [
+	educations : {
+		"schools" : [
+			{
+				"school" : "UC Berkley",
+				"location" : "Berkley, CA",
+				"degree" : "Ph.D.",
+				"major" : "Computer Science"
+			},
+			{
+				"school" : "MIT",
+				"location" : "Cambridge, MA",
+				"degree" : "BS",
+				"major" : "Electrical Engineering"
+			}
+		],
+		"Online Course" : ["Udacity"]
+	},
+
+	projects : [
 		{
 			"title" : "AI",
 			"dates" : "1/7/2016 - present",
 			"description" : "Building an AI",
-			"images" : ["images\\ai1.jpg"]
+			"images" : ["images/ai1.jpg"]
 		},
 		{
 			"title" : "Search engine",
 			"dates" : "3/15/2012 - 4/5/2016",
 			"description" : "Built a search engine from scratch",
-			"images" : ["images\\se1.png", "images\\se2.jpg"]
+			"images" : ["images/se1.png", "images/se2.jpg"]
 		}
 	]
 };
 
-projects.display = function() {
-	projects.projects.forEach(function (project) {
-		$("#projects").append(HTMLprojectStart);
-
-		$(".project-entry:last").append(HTMLprojectTitle.replace(placeholder, project.title));
-		$(".project-entry:last").append(HTMLprojectDates.replace(placeholder, project.dates));
-		$(".project-entry:last").append(HTMLprojectDescription.replace(placeholder, project.description));
-
-		project.images.forEach(function (image) {
-			$(".project-entry:last").append(HTMLprojectImage.replace(placeholder, image));
+var view = {
+	init : function() {
+		// populate works
+		octopus.getAllWorks().forEach(function(work) {		
+			$("#workExperience").append(HTMLworkStart);
+			$(".work-entry:last").append(HTMLworkEmployer.replace(placeholder, work.employer));
+			$(".work-entry:last").append(HTMLworkTitle.replace(placeholder, work.title));
+			$(".work-entry:last").append(HTMLworkDates.replace(placeholder, work.dates));
+			$(".work-entry:last").append(HTMLworkLocation.replace(placeholder, work.location));
+			$(".work-entry:last").append(HTMLworkDescription.replace(placeholder, work.description));
 		});
-	});
-}
 
-function displayWork(works) {
-	works.jobs.forEach(function(work) {		
-		$("#workExperience").append(HTMLworkStart);
-		$(".work-entry:last").append(HTMLworkEmployer.replace(placeholder, work.employer));
-		$(".work-entry:last").append(HTMLworkTitle.replace(placeholder, work.title));
-		$(".work-entry:last").append(HTMLworkDates.replace(placeholder, work.dates));
-		$(".work-entry:last").append(HTMLworkLocation.replace(placeholder, work.location));
-		$(".work-entry:last").append(HTMLworkDescription.replace(placeholder, work.description));
-	});
-}
+		// populate projects
+		octopus.getAllProjects().forEach(function (project) {
+			$("#projects").append(HTMLprojectStart);
+			$(".project-entry:last").append(HTMLprojectTitle.replace(placeholder, project.title));
+			$(".project-entry:last").append(HTMLprojectDates.replace(placeholder, project.dates));
+			$(".project-entry:last").append(HTMLprojectDescription.replace(placeholder, project.description));
+
+			project.images.forEach(function (image) {
+				$(".project-entry:last").append(HTMLprojectImage.replace(placeholder, image));
+			});
+		});
+
+		// populate bio
+		var bio = octopus.getBio();
+		$("#header").append(HTMLheaderName.replace(placeholder, bio.name));
+
+		if ("skills" in bio && bio.skills.length > 0) {
+
+			$("#header").append(HTMLskillsStart);
+
+			for (skill in bio.skills) {
+				$("#skills").append(HTMLskills.replace(placeholder, bio.skills[skill]));
+			}
+		}
+
+		// init button and map
+		$("#main").append(internationalizeButton);
+
+		$("#mapDiv").append(googleMap);
+	}
+};
+
+var octopus = {
+	init : function() {
+		view.init();
+	},
+
+	getAllWorks : function() {
+		return model.jobs;
+	},
+
+	getAllProjects : function() {
+		return model.projects;
+	},
+
+	getBio : function() {
+		return model.bio;
+	}
+};
+
+octopus.init();
 
 function inName(name) {
 	var names = name.trim().split(" ");
@@ -123,24 +154,7 @@ function inName(name) {
 	return names.join(" ");
 }
 
-$("#main").append(internationalizeButton);
 
-$("#mapDiv").append(googleMap);
-
-$("#header").append(HTMLheaderName.replace(placeholder, bio.name));
-
-if ("skills" in bio && bio.skills.length > 0) {
-
-	$("#header").append(HTMLskillsStart);
-
-	for (skill in bio.skills) {
-		$("#skills").append(HTMLskills.replace(placeholder, bio.skills[skill]));
-	}
-}
-
-displayWork(works);
-
-projects.display();
 
 
 
